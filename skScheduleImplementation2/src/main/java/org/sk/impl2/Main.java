b501dbe34e4a313f1c05807e272bb0ff7ae282d8
LanaLazarevic <llazarevic6021rn@raf.rs>
2023-11-13 14:53:50 +0100

    Pretraga zauzetih termina

--- skScheduleImplementation2/src/main/java/org/sk/impl2/Main.java
+++ skScheduleImplementation2/src/main/java/org/sk/impl2/Main.java
@@ -1,7 +1,34 @@
 package org.sk.impl2;
 
+import org.sk.impl2.implementacija.Implementation2;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Implementation2 implementacija = new Implementation2();
+
+        implementacija.loadData("termini.csv", "config.txt");
+
+        System.out.println("\nOPIS PROSTORA (METADATA): \n" + implementacija);
+
+        System.out.println("\nUcitan raspored: \n" + implementacija.ispisRasporeda(implementacija.getRaspored()));
+        Map<String, String> a = new HashMap<>();
+        a.put("Dan", "petak");
+        a.put("Predmet", "OOP");
+        implementacija.addTermin("12/11/2023 12:00", "01/13/2024 14:00", "RAF3", a);
+        System.out.println("\nRaspored: \n" + implementacija.ispisRasporeda(implementacija.getRaspored()));
+        Map<String, String> b = new HashMap<>();
+        b.put("racunar", "NE");
+        b.put("mikrofon", "DA");
+        implementacija.addRoom("RAF30", b);
+        System.out.println("\nUcionice: \n" + implementacija.getSveSobe());
+        implementacija.deleteTermin("12/11/2023 12:00", "01/13/2024 14:00", "RAF3");
+        System.out.println("\nRaspored: \n" + implementacija.ispisRasporeda(implementacija.getRaspored()));
+        Map<String, String> c = new HashMap<>();
+        c.put("Profesor", "Bojana Dimic Surla");
+        //c.put("Predmet", "UUP");
+        System.out.println("\nPretraga: \n" + implementacija.ispisRasporeda(implementacija.pretragaTermina("10/30/2023", null, null, null,null,c,null)));
     }
 }
--- skScheduleImplementation2/src/main/java/org/sk/impl2/implementacija/Implementation2.java
+++ skScheduleImplementation2/src/main/java/org/sk/impl2/implementacija/Implementation2.java
@@ -1,17 +1,22 @@
 package org.sk.impl2.implementacija;
 
 import Specifikacija.SpecifikacijaRasporeda;
+import Termin.Termin;
+import Termin.Room;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Implementation2 extends SpecifikacijaRasporeda {
-   /* @Override
+    @Override
     public void addTermin(String start, String end, String ucionica, Map<String, String> additional) {
-        Termin termin = new Termin(LocalDateTime.parse(start, DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm")),
-                LocalDateTime.parse(end, DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm")), nadjisobupoImenu(ucionica), additional);
+        Termin termin = new Termin(LocalDateTime.parse(start, getFormatDatuma()),
+                LocalDateTime.parse(end, getFormatDatuma()), nadiSobuPoImenu(ucionica), additional);
         boolean flag = true;
         for (Termin t:
                 getRaspored()) {
@@ -27,9 +32,52 @@ public class Implementation2 extends SpecifikacijaRasporeda {
     }
 
 
-    @Override
+   /* @Override
     public List<Termin> pretragaTermina(String kriterijum, boolean zauzetost) {
         return super.pretragaTermina(kriterijum, zauzetost);
+    }*/
+
+    /*@Override
+    public List<Termin> pretragaTermina(String start, String end, String roomName, Map<String, String> additional, String dayOfTheWeek) {
+        List<Termin> termini = new ArrayList<>();
+        for (Termin t:
+             getRaspored()) {
+
+        }
+        return termini;
+    }*/
+
+    @Override
+    public List<Termin> pretragaTermina(String start, String end, String vremeod, String vremedo, String roomName, Map<String, String> additional, String dayOfTheWeek) {
+        List<Termin> termini = new ArrayList<>();
+        LocalDate startd=null, endd=null;
+        LocalTime  pocetak=null, kraj=null;
+        if(start!=null)
+            startd = LocalDate.parse(start,DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+        if(end!=null)
+            endd = LocalDate.parse(end,DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+        if(vremeod!=null)
+            pocetak = LocalTime.parse(vremeod, DateTimeFormatter.ofPattern("HH:mm"));
+        if(vremedo!=null)
+            kraj = LocalTime.parse(vremedo, DateTimeFormatter.ofPattern("HH:mm"));
+        Room room = nadiSobuPoImenu(roomName);
+        for (Termin t:
+                getRaspored()) {
+            if(((start==null && end == null) || ( (startd!=null) && t.getStart().isBefore(startd.atStartOfDay()) && t.getEnd().isAfter(startd.atStartOfDay()) && t.getAdditional().get("Dan").equalsIgnoreCase(nadjiDan(startd.getDayOfWeek().toString()))))
+                    && ((pocetak == null && kraj==null) || ( (pocetak!=null && kraj!=null) && t.getStart().getHour() == pocetak.getHour() && t.getStart().getMinute() == pocetak.getMinute()  && t.getEnd().getHour() == kraj.getHour() && t.getEnd().getMinute() == kraj.getMinute()))
+                    && (roomName == null || (t.getRoom().equals(room)))
+                    && (additional==null || (ispitajAdditional(t, additional))) ){
+                termini.add(t);
+            } else if( ((start==null && end == null) || ( (startd!=null && endd!=null) && t.getStart().isBefore(startd.atStartOfDay()) && t.getEnd().isAfter(endd.atStartOfDay())))
+                    && (dayOfTheWeek == null || dayOfTheWeek.equalsIgnoreCase(t.getAdditional().get("Dan")))
+                    && (pocetak == null || ( (pocetak!=null && kraj!=null) && t.getStart().getHour() == pocetak.getHour() && t.getStart().getMinute() == pocetak.getMinute()  && t.getEnd().getHour() == kraj.getHour() && t.getEnd().getMinute() == kraj.getMinute()))
+                    && (roomName == null || (t.getRoom().equals(room)))
+                    && (additional==null || (ispitajAdditional(t, additional))) ){
+                termini.add(t);
+            }
+
+        }
+        return termini;
     }
 
     @Override
@@ -42,8 +90,8 @@ public class Implementation2 extends SpecifikacijaRasporeda {
                 "Dan=" + termin.getAdditional().get("Dan") +
                 ", Vreme=" + termin.getStart().format(DateTimeFormatter.ofPattern("HH:mm")) + "-" +
                 termin.getEnd().format(DateTimeFormatter.ofPattern("HH:mm")) +
-                ", Od=" + termin.getStart().format(DateTimeFormatter.ofPattern("MM/dd/YYYY")) +
-                ", Do=" + termin.getEnd().format(DateTimeFormatter.ofPattern("MM/dd/YYYY")) +
+                ", Od=" + termin.getStart().format(DateTimeFormatter.ofPattern("MM/dd/yyyy")) +
+                ", Do=" + termin.getEnd().format(DateTimeFormatter.ofPattern("MM/dd/yyyy")) +
                 ", Room=" + ispisiRoom(termin.getRoom()) +
                 ispisiAdditional(termin.getAdditional())+
                 '\n';
@@ -79,5 +127,33 @@ public class Implementation2 extends SpecifikacijaRasporeda {
         }
         return sb.toString();
     }
-*/
+    public String nadjiDan(String dan){
+        if(dan.equalsIgnoreCase("monday")){
+            return "ponedeljak";
+        } else if(dan.equalsIgnoreCase("tuesday")){
+            return "utorak";
+        } else if(dan.equalsIgnoreCase("wednesday")){
+            return "sreda";
+        } else if(dan.equalsIgnoreCase("thursday")){
+            return "cetvrtak";
+        } else if(dan.equalsIgnoreCase("saturday")){
+            return "subota";
+        } else {
+            return "nedelja";
+        }
+
+    }
+
+    public boolean ispitajAdditional(Termin t, Map<String, String> k){
+        boolean flag = false;
+        for (Map.Entry<String, String> s:
+            k.entrySet()) {
+            if(t.getAdditional().containsKey(s.getKey())){
+                flag = t.getAdditional().get(s.getKey()).equalsIgnoreCase(s.getValue());
+            }
+            if(!flag)
+                break;
+        }
+        return flag;
+    }
 }
